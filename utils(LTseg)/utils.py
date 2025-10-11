16c3f75875cf764f1e973060884a4012670f489b
XWei98 <shawz1010@outlook.com>
2025-10-11 11:06:36 +0800

    Update utils.py

--- utils(LTseg)/utils.py
+++ utils(LTseg)/utils.py
@@ -1,10 +1,7 @@
 import numpy as np
 import os
 from PIL import Image
-# ---------------------------------------------------------#
-#   将图像转换成RGB图像，防止灰度图在预测时报错。
-#   代码仅仅支持RGB图像的预测，所有其它类型的图像都会转化成RGB
-# ---------------------------------------------------------#
+
 def cvtColor(image):
     if len(np.shape(image)) == 3 and np.shape(image)[2] == 3:
         return image
@@ -12,9 +9,7 @@ def cvtColor(image):
         image = image.convert('RGB')
         return image
 
-# ---------------------------------------------------#
-#   对输入图像进行resize
-# ---------------------------------------------------#
+
 def resize_image(image, size):
     iw, ih = image.size
     w, h = size
@@ -30,9 +25,7 @@ def resize_image(image, size):
     return new_image, nw, nh
 
 
-# ---------------------------------------------------#
-#   获得学习率
-# ---------------------------------------------------#
+
 def get_lr(optimizer):
     for param_group in optimizer.param_groups:
         return param_group['lr']
