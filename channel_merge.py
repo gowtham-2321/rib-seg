cac3d47c14cf3432f53a247fae5bfabd0bb79eee
XWei98 <shawz1010@outlook.com>
2025-10-11 11:08:44 +0800

    Update channel_merge.py

--- channel_merge.py
+++ channel_merge.py
@@ -4,14 +4,14 @@ import numpy as np
 from tqdm import tqdm
 
 
-path1 = '/newhome/zhaoxiaowei/Docode/LVSCiT/results/loss_2023_07_18_11_09/pred_image'
-target1 = "/newhome/zhaoxiaowei/Docode/LVSCiT/results/loss_2023_07_18_11_09/mergerchannel5"
-target2 = "/newhome/zhaoxiaowei/Docode/LVSCiT/results/loss_2023_07_18_11_09/mergerchannel6"
+path1 = '/path/to/pred_image'
+target1 = "/path/to/mergerchannel1"
+target2 = "/path/to/mergerchannel2"
 if not os.path.exists(target1):
     os.makedirs(target1)
 if not os.path.exists(target2):
     os.makedirs(target2)
-for path2 in tqdm(os.listdir("/newhome/zhaoxiaowei/Docode/LVSCiT/data/nd")):
+for path2 in tqdm(os.listdir("/path/to/test/img")):
     img = np.zeros([448,448,3],dtype = np.uint8)
     color = [(255,0,255),(60,255,125),(0,0,255),(125,255,60),
              (255,60,125),(255,125,60),(60,125,0),(60,0,125),
@@ -34,12 +34,11 @@ for path2 in tqdm(os.listdir("/newhome/zhaoxiaowei/Docode/LVSCiT/data/nd")):
         img[:,:,2] = cv2.bitwise_or(img[:,:,2],color[i][2] * img1)
 
     cv2.imwrite(os.path.join(target1,path2),img)
-    #print('img',path2)
-    #print(os.path.join("/media/20TB/madexin3/bingbianshuju",path2))
-    #print(0)
-    img1 = cv2.imread(os.path.join("/newhome/zhaoxiaowei/Docode/LVSCiT/data/nd", path2), 0)
+ 
+    img1 = cv2.imread(os.path.join("/path/to/test/img", path2), 0)
 
     img1 = cv2.resize(img1,(448,448))
     img1 = cv2.cvtColor(img1, cv2.COLOR_GRAY2BGR)
     img =  cv2.addWeighted(img,0.5,img1,0.8,0)
     cv2.imwrite(os.path.join(target2,path2),img)
+
