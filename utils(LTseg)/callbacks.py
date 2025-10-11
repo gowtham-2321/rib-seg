d39ab48b8da20fc8d14e77d6494c7cbf3b3a63ca
XWei98 <shawz1010@outlook.com>
2025-10-11 11:06:19 +0800

    Update callbacks.py

--- utils(LTseg)/callbacks.py
+++ utils(LTseg)/callbacks.py
@@ -87,21 +87,13 @@ class EvalCallback():
                 f.write("\n")
 
     def get_miou_png(self, image):
-        # ---------------------------------------------------------#
-        #   在这里将图像转换成RGB图像，防止灰度图在预测时报错。
-        #   代码仅仅支持RGB图像的预测，所有其它类型的图像都会转化成RGB
-        # ---------------------------------------------------------#
+
         image = cvtColor(image)
         orininal_h = np.array(image).shape[0]
         orininal_w = np.array(image).shape[1]
-        # ---------------------------------------------------------#
-        #   给图像增加灰条，实现不失真的resize
-        #   也可以直接resize进行识别
-        # ---------------------------------------------------------#
+
         image_data, nw, nh = resize_image(image, (self.input_shape[1], self.input_shape[0]))
-        # ---------------------------------------------------------#
-        #   添加上batch_size维度
-        # ---------------------------------------------------------#
+
         image_data = np.expand_dims(np.transpose(preprocess_input(np.array(image_data, np.float32)), (2, 0, 1)), 0)
 
         with torch.no_grad():
@@ -109,26 +101,15 @@ class EvalCallback():
             if self.cuda:
                 images = images.cuda()
 
-            # ---------------------------------------------------#
-            #   图片传入网络进行预测
-            # ---------------------------------------------------#
             pr = self.net(images)[0]
-            # ---------------------------------------------------#
-            #   取出每一个像素点的种类
-            # ---------------------------------------------------#
+
             pr = F.softmax(pr.permute(1, 2, 0), dim=-1).cpu().numpy()
-            # --------------------------------------#
-            #   将灰条部分截取掉
-            # --------------------------------------#
+
             pr = pr[int((self.input_shape[0] - nh) // 2): int((self.input_shape[0] - nh) // 2 + nh), \
                  int((self.input_shape[1] - nw) // 2): int((self.input_shape[1] - nw) // 2 + nw)]
-            # ---------------------------------------------------#
-            #   进行图片的resize
-            # ---------------------------------------------------#
+
             pr = cv2.resize(pr, (orininal_w, orininal_h), interpolation=cv2.INTER_LINEAR)
-            # ---------------------------------------------------#
-            #   取出每一个像素点的种类
-            # ---------------------------------------------------#
+
             pr = pr.argmax(axis=-1)
 
         image = Image.fromarray(np.uint8(pr))
@@ -145,19 +126,15 @@ class EvalCallback():
                 os.makedirs(pred_dir)
             print("Get miou.")
             for image_id in tqdm(self.image_ids):
-                # -------------------------------#
-                #   从文件中读取图像
-                # -------------------------------#
+
                 image_path = os.path.join(self.dataset_path, "VOC2007/JPEGImages/" + image_id + ".jpg")
                 image = Image.open(image_path)
-                # ------------------------------#
-                #   获得预测txt
-                # ------------------------------#
+  
                 image = self.get_miou_png(image)
                 image.save(os.path.join(pred_dir, image_id + ".png"))
 
             print("Calculate miou.")
-            _, IoUs, _, _ = compute_mIoU(gt_dir, pred_dir, self.image_ids, self.num_classes, None)  # 执行计算mIoU的函数
+            _, IoUs, _, _ = compute_mIoU(gt_dir, pred_dir, self.image_ids, self.num_classes, None)  
             temp_miou = np.nanmean(IoUs) * 100
 
             self.mious.append(temp_miou)
@@ -182,3 +159,4 @@ class EvalCallback():
 
             print("Get miou done.")
             shutil.rmtree(self.miou_out_path)
+
